974da82b00f8715bfda7193f34e2a97c156a13a8
Tomasz Kapela <tomasz.kapela@uj.edu.pl>
2021-12-30 14:19:31 +0100

    merge

--- talks/code/maskmov.asm
+++ talks/code/maskmov.asm
@@ -17,25 +17,25 @@ compute:
    movaps xmm7, xmm1
    cmpnleps xmm7, xmm0
     
-   ; then 
-   movaps xmm2, xmm1
-   addps  xmm2, xmm4
+   ; then (tab[i]+4)*sqrt(tab[i])
+  ; movaps xmm2, xmm1
+  ; addps  xmm2, xmm4
    movaps xmm6, xmm1
    sqrtps xmm6, xmm6
-   mulps  xmm2, xmm6
+ ;  mulps  xmm2, xmm6
    movups [rsi], xmm2
    
-   ;else 
+   ;else (tab[i]*tab[i])*sqrt(-tab[i])
    movaps xmm3, xmm1
    mulps  xmm3, xmm1
    movaps xmm5, xmm0
    subps  xmm5, xmm1
    sqrtps xmm5, xmm5
    mulps  xmm3, xmm5
-   vmaskmovps [rsi], xmm7, xmm3
+;   vmaskmovps [rsi], xmm7, xmm3
 
    add rsi, 16
-   sub edi, 3    
+   sub edi, 4    
    
 .start:
    cmp edi,3
--- talks/code/maskmov.cpp
+++ talks/code/maskmov.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 extern "C" void compute(int n, float * tab);
@@ -9,8 +10,11 @@ int main(){
 	              -2.0, 4.5, -4.2, 1.9};
 	              
   compute(8, tab);
+  int i =0;
   for(auto x : tab){
 	  cout << x <<"  ";
+	  cout << ( (tab[i] > 0) ? (tab[i]+4)*sqrt(tab[i]) : (tab[i]*tab[i])*sqrt(-tab[i])) << "\n"; 
+      i++;
   } 
   return 0;
 }
